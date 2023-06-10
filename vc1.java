20e93dc758eef78ec9cc20f675bca6cc8b8dd9b3
Vibha Hathwar <86974808+Vibha-Hathwar@users.noreply.github.com>
2023-06-10 21:25:13 +0530

    visiting card
    Create an application to design a Visiting Card. The Visiting card should have a company 
    logo at the top right corner. The company name should be displayed in Capital letters, 
    aligned to the center. Information like the name of the employee, job title, phone number, 
    address, email, fax and the website address is to be displayed. Insert a horizontal line 
    between the job title and the phone number.

---
+++ vc1.java
@@ -0,0 +1,16 @@
+MainActivity.java
+
+package com.example.visitingcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
